31f4e815f48be8e1a7099fd3e741d3e1ef0a9cd4
kager1 <katonageri@gmail.com>
2020-04-20 18:19:22 +0200

    almost working

--- GreenFox/ToDoList/scripts.js
+++ GreenFox/ToDoList/scripts.js
@@ -1,15 +1,29 @@
-let countOfitems = 0
+//let countOfitems = 0
+let liststring = []
+let listobject = ""
 
-$("#addone").click(() =>{
-    countOfitems++
-    liststring = $("#listitem").val();
-    console.log(liststring)
-    $("#main").append('<div><h2>' + liststring + '</div></h2>')
-    $("#listitem").val("")
-})
+$("#addone").click(() => {
+    //    countOfitems++
+    //    liststring = [$("#listitem").val()];
+    if ($("#listitem").val() == 0) {
+        console.log("nothing to add");
+    }
+    else {
+        liststring.push($("#listitem").val());
+        listobject = $("#listitem").val()
+        console.log(listobject);
+        $("#listitem").val("")
+        //$("ul").append(`<li class="">${listobject}</li>`)
+        $("ul").append('<li class="">' + liststring.slice(-1) + '</li>')
 
-/* $("#addone").on('click', () => {
-    countOfitems++
-    let liststring = $("#listitem").val();
-    $("ul").append("<li>"liststring"</li>")
-)}; */
+    };
+
+    $("li").click(function () {
+            $(this).toggleClass("stroked" );
+        }
+        //$("li").toggleClass( "stroked", addOrRemove )
+        //$li.toggleClass('stroked')
+        //$(this).css("text-decoration", "line-through")
+    )
+
+});
